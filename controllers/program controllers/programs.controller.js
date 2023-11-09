2355f283909866ed4de4058c8a80d8ba71e0cc0a
clintjeff2 <clintjeff2@gmail.com>
2023-11-09 05:17:40 +0100

    Created the program controllers for creating and getting programs

---
+++ controllers/program controllers/programs.controller.js
@@ -0,0 +1,14 @@
+const sendResponse = require('../../utilities/sendResponse');
+const Program = require('./../../models/programs.model');
+const ErrorApi = require('./../../utilities/ErrorApi');
+const catchAsync = require('./../../utilities/catchAsync');
+
+exports.createProgram = catchAsync(async (req, res, next) => {
+	const { name, director, deputyDirector } = req.body;
+
+	const program = await Program.create({ name, director, deputyDirector });
+
+	if (!program) return next(new ErrorApi('Program not created', 400));
+
+	sendResponse(res, 'success', 201, program);
+});
