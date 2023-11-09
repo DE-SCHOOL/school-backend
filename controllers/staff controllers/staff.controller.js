7a2e20e39eb85e81812d1a90cb9d9a0b6edde1c7
clintjeff2 <clintjeff2@gmail.com>
2023-11-09 05:23:12 +0100

    Created the staff controller for creating and getting all staff

---
+++ controllers/staff controllers/staff.controller.js
@@ -0,0 +1,13 @@
+const ErrorApi = require('./../../utilities/ErrorApi');
+const sendResponse = require('./../../utilities/sendResponse');
+const catchAsync = require('./../../utilities/catchAsync');
+const Staff = require('./../../models/staff.model');
+
+exports.getAllStaffs = catchAsync(async (req, res, next) => {
+	const staffs = await Staff.find({});
+
+	console.log('This is nonsense');
+	if (!staffs) return next(new ErrorApi('Staffs were not found'));
+	console.log('this is not good the second');
+	sendResponse(res, 'success', 200, staffs);
+});
