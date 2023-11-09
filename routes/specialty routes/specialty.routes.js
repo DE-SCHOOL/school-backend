89d79622b5c07d75e647531a0ead252f132222a7
clintjeff2 <clintjeff2@gmail.com>
2023-11-09 05:26:19 +0100

    Created the specialty routes

---
+++ routes/specialty routes/specialty.routes.js
@@ -0,0 +1,18 @@
+const authController = require('./../../controllers/authentication/auth.controller');
+const specialtyController = require('./../../controllers/specialty controllers/specialty.controller');
+
+const express = require('express');
+
+const router = express.Router();
+
+// router.use(authController.protect);
+
+router
+	.route('/')
+	.post(
+		authController.restrictTo('admin', 'director', 'hod'),
+		specialtyController.createSpecialty
+	)
+	.get(specialtyController.getAllSpecialties);
+
+module.exports = router;
