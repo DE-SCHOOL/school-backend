1d078a1fc8f29c93f7dce3a72ac1e2b5976e3c46
clintjeff2 <clintjeff2@gmail.com>
2023-11-09 05:18:19 +0100

    Created the department controller for creating and getting all departments

---
+++ controllers/department controllers/department.controller.js
@@ -0,0 +1,14 @@
+const sendResponse = require('../../utilities/sendResponse');
+const Department = require('./../../models/department.model');
+const ErrorApi = require('./../../utilities/ErrorApi');
+const catchAsync = require('./../../utilities/catchAsync');
+
+exports.createDepartment = catchAsync(async (req, res, next) => {
+	const { name, program, hod } = req.body;
+
+	const department = await Department.create({ name, hod, program });
+
+	if (!department) return next(new ErrorApi('Department not created', 400));
+
+	sendResponse(res, 'success', 201, department);
+});
