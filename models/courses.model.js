feafefd4f1235816770cbbca5fab11a2f53f1ce7
clintjeff2 <clintjeff2@gmail.com>
2023-11-30 11:02:07 +0100

    Creaded the courses model

---
+++ models/courses.model.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+
+const courseSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: [true, 'A course must have a name'],
+	},
+	specialty: [
+		{
+			type: mongoose.Types.ObjectId,
+			ref: 'specialty',
+			required: [true, 'A course must be attributed to atleast one specialty'],
+		},
+	],
+	code: {
+		type: String,
+		required: [true, 'A course must have a code'],
+		unique: true,
+	},
+	semester: {
+		type: String,
+		enum: {
+			values: ['s1', 's2'],
+			message: 'A semester must either be s1 or s2',
+		},
+		required: [true, 'A course belongs to a semester'],
+	},
+	levels: [
+		{
+			type: Number,
+			enum: {
+				values: [200, 300, 400, 500],
+				message: 'A level must either be 200, 300, 400, or 500',
+			},
+			required: [true, 'A course is tought in a particular class level'],
+		},
+	],
+	credit_value: {
+		type: Number,
+		required: [true, 'A course must have a credit value'],
+	},
+	status: {
+		type: String,
+		required: [true, 'A course must have a status'],
+		enum: {
+			values: ['compulsory', 'elective'],
+			message: 'A course must either be compulsory or be an elective',
+		},
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now(),
+	},
+});
+
+const Course = mongoose.model('course', courseSchema);
+module.exports = Course;
