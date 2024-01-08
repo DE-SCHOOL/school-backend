491e8e97fcf18cfb9ea20f5f5f0f61bf75fc9d4e
clintjeff2 <clintjeff2@gmail.com>
2024-01-08 18:37:59 +0100

    Made the edit department endpoint route used to access the editDepartment controller function

--- routes/department routes/department.routes.js
+++ routes/department routes/department.routes.js
@@ -1,5 +1,6 @@
 const authController = require('./../../controllers/authentication/auth.controller');
 const departmentController = require('./../../controllers/department controllers/department.controller');
+const RIGHTS = require('./../../utilities/restrict');
 
 const express = require('express');
 
@@ -9,10 +10,24 @@ router.use(authController.protect);
 
 router
 	.route('/')
-	.get(departmentController.getAllDepartments)
+	.get(
+		authController.restrictTo(...RIGHTS.TO_ALL_STAFF),
+		departmentController.getAllDepartments
+	)
 	.post(
-		authController.restrictTo('admin', 'director'),
+		authController.restrictTo(...RIGHTS.TO_MAIN_ADMIN),
 		departmentController.createDepartment
+	);
+
+router
+	.route('/:id')
+	.get(
+		authController.restrictTo(...RIGHTS.TO_ALL_STAFF),
+		departmentController.getDepartment
+	)
+	.patch(
+		authController.restrictTo(...RIGHTS.TO_MAIN_ADMIN),
+		departmentController.editDepartment
 	);
 
 module.exports = router;
