798f4f5f3f5d943123107eaf09440efee637818b
clintjeff2 <clintjeff2@gmail.com>
2024-01-08 18:38:55 +0100

    Wrote controller to edit a particular program information and made the edit program endpoint route used to access the editProgram controller function

--- controllers/program controllers/programs.controller.js
+++ controllers/program controllers/programs.controller.js
@@ -18,3 +18,28 @@ exports.getPrograms = catchAsync(async (req, res, next) => {
 
 	sendResponse(res, 'success', 200, programs);
 });
+
+exports.getProgram = catchAsync(async (req, res, next) => {
+	const { id } = req.params;
+
+	const program = await Program.findById(id);
+
+	if (!program) return next(new ErrorApi('No such program', 404));
+
+	sendResponse(res, 'success', 200, program);
+});
+
+exports.editProgram = catchAsync(async (req, res, next) => {
+	const { id } = req.params;
+	const { name, director, deputyDirector } = req.body;
+
+	const program = await Program.findByIdAndUpdate(
+		id,
+		{ name, director, deputyDirector },
+		{ new: true, runValidators: true }
+	);
+
+	if (!program) return next(new ErrorApi('Program not created', 404));
+
+	sendResponse(res, 'success', 200, program);
+});
