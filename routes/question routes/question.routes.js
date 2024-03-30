8801446bbef11437298af97e4464533716090cb9
clintjeff2 <clintjeff2@gmail.com>
2024-03-30 18:27:30 +0100

    Created the file for question routes. Added routes there to get, create, edit and delete a question

---
+++ routes/question routes/question.routes.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const RIGHTS = require('./../../utilities/restrict');
+const authController = require('./../../controllers/authentication/auth.controller');
+const questionController = require('./../../controllers/question controller/question.controller');
+
+const router = express.Router();
+
+router
+	.route('/:tokenID')
+	.post(
+		authController.protect,
+		authController.restrictTo(...RIGHTS.TO_ALL_OFFICE_ADMIN),
+		questionController.createQuestion
+	)
+	.get(
+		authController.protect,
+		authController.restrictTo(...RIGHTS.TO_ALL_OFFICE_ADMIN),
+		questionController.getAllQuestions
+	);
+
+router
+	.route('/:questionID/:tokenID')
+	.get(
+		authController.protect,
+		authController.restrictTo(...RIGHTS.TO_ALL_OFFICE_ADMIN),
+		questionController.getQuestion
+	)
+	.patch(
+		authController.protect,
+		authController.restrictTo(...RIGHTS.TO_ALL_OFFICE_ADMIN),
+		questionController.editQuestion
+	)
+	.delete(
+		authController.protect,
+		authController.restrictTo(...RIGHTS.TO_ALL_OFFICE_ADMIN),
+		questionController.deleteQuestion
+	);
+
+module.exports = router;
