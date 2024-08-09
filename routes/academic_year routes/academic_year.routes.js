027b8bb228755e10465e0cf993392bb3e87f0a64
clintjeff2 <clintjeff2@gmail.com>
2024-08-09 17:47:59 +0100

    Created routes for get all years, create a new year, and get the current academic year

---
+++ routes/academic_year routes/academic_year.routes.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const authController = require('../../controllers/authentication/auth.controller');
+const AcademicYear = require('./../../controllers/academic_year controller/academic_year.controller');
+
+const router = express.Router();
+
+router
+	.route('/:tokenID')
+	.get(authController.protect, AcademicYear.getAcademicYears)
+	.post(authController.protect, AcademicYear.createAcademicYear);
+router
+	.route('/:id/:tokenID')
+	.patch(authController.protect, AcademicYear.updateAcademicYears);
+
+module.exports = router;
