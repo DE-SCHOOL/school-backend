93f1a23e70451c4ef1fd8544db4ea96fa07857c8
clintjeff2 <clintjeff2@gmail.com>
2024-08-09 17:50:01 +0100

    Created the academic controller for creating a year, getting all years, and updating years

---
+++ controllers/academic_year controller/academic_year.controller.js
@@ -0,0 +1,33 @@
+const AcademicYear = require('./../../models/academic_year.model');
+const ErrorApi = require('./../../utilities/ErrorApi');
+const sendResponse = require('./../../utilities/sendResponse');
+const catchAsync = require('./../../utilities/catchAsync');
+
+exports.createAcademicYear = catchAsync(async (req, res, next) => {
+	const year = req.body.academicYear;
+
+	if (!year) return ErrorApi('Enter the Academic Year', 400);
+
+	const academicYear = await AcademicYear.create({ schoolYear: year });
+	sendResponse(res, 'success', 201, academicYear);
+});
+
+exports.getAcademicYears = catchAsync(async (req, res, next) => {
+	const data = await AcademicYear.find({});
+	sendResponse(res, 'success', 200, data);
+});
+
+exports.updateAcademicYears = catchAsync(async (req, res, next) => {
+	const id = req.params.id;
+	const data = await AcademicYear.updateMany(
+		{ isCurrent: true },
+		{ isCurrent: false },
+		{ new: true }
+	);
+
+	const newUpdate = await AcademicYear.findByIdAndUpdate(id, {
+		isCurrent: true,
+	});
+
+	sendResponse(res, 'success', 200, newUpdate);
+});
