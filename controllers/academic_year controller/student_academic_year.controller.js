489c7ebdfc23e2014b2f0e40220e79724fb8025a
clintjeff2 <clintjeff2@gmail.com>
2024-08-16 10:23:12 +0100

    Added method to promote students in bulk

--- controllers/academic_year controller/student_academic_year.controller.js
+++ controllers/academic_year controller/student_academic_year.controller.js
@@ -7,6 +7,7 @@ const Student = require('../../models/students.model');
 exports.createStudentAcademicYearBulk = catchAsync(async (req, res, next) => {
 	const { studentIDs, toYearID } = req.body;
 
+	console.log(studentIDs);
 	if (!studentIDs || studentIDs.length === 0) {
 		return next(new ErrorAPI('Please provide students to be promoted', 400));
 	}
@@ -19,7 +20,7 @@ exports.createStudentAcademicYearBulk = catchAsync(async (req, res, next) => {
 		});
 	}
 
-	sendResponse(res, 201, 'success', []);
+	sendResponse(res, 'success', 201, []);
 });
 
 exports.promoteStudent = catchAsync(async (req, res, next) => {
@@ -35,7 +36,7 @@ exports.promoteStudent = catchAsync(async (req, res, next) => {
 		{
 			level: newClass,
 		},
-		{ new: true }
+		{ new: true, runValidators: true }
 	);
 
 	const studentAcademicYear = await StudentAcademicYear.create({
@@ -48,6 +49,41 @@ exports.promoteStudent = catchAsync(async (req, res, next) => {
 	);
 
 	let allStudents = students.map((stud) => stud.student);
+
+	sendResponse(res, 'success', 201, allStudents);
+});
+
+exports.promoteStudentsBulk = catchAsync(async (req, res, next) => {
+	// const { studentID, toYear, newClass } = req.body;
+	const { students } = req.body;
+	const academicYear = req.params.academicYearID;
+
+	if (students.length === 0 || !students) {
+		return next(
+			new ErrorAPI('Please provide student and academic year information', 400)
+		);
+	}
+
+	for (i = 0; i < students.length; i++) {
+		await Student.findByIdAndUpdate(
+			students[i].studentID,
+			{
+				level: students[i].newClass,
+			},
+			{ new: true, runValidators: true }
+		);
+
+		await StudentAcademicYear.create({
+			student: students[i].studentID,
+			academicYear: students[i].toYear,
+		});
+	}
+
+	const studentsData = await StudentAcademicYear.find({
+		academicYear,
+	}).populate('student');
+
+	let allStudents = studentsData.map((stud) => stud.student);
 
 	sendResponse(res, 'success', 201, allStudents);
 });
