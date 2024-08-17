9afa984969b1ff482f198777e96203b0fc71efd6
clintjeff2 <clintjeff2@gmail.com>
2024-08-17 17:37:55 +0100

    Edited the get students per academic year function to automatically populate all sub fields from specialty to director and deputy director, and also edited promote students in bulk to sort the students per level

--- controllers/academic_year controller/student_academic_year.controller.js
+++ controllers/academic_year controller/student_academic_year.controller.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const StudentAcademicYear = require('./../../models/student_academic_year.model');
 const ErrorAPI = require('./../../utilities/ErrorApi');
 const sendResponse = require('./../../utilities/sendResponse');
@@ -83,7 +84,9 @@ exports.promoteStudentsBulk = catchAsync(async (req, res, next) => {
 
 	const studentsData = await StudentAcademicYear.find({
 		academicYear,
-	}).populate('student');
+	})
+		.populate('student')
+		.sort({ level: 1 });
 
 	let allStudents = studentsData.map((stud) => {
 		const student = stud.student;
@@ -95,11 +98,109 @@ exports.promoteStudentsBulk = catchAsync(async (req, res, next) => {
 });
 
 exports.getStudentPerAcademicYear = catchAsync(async (req, res, next) => {
-	const academicYear = req.params.academicYearID;
+	const { academicYearID } = req.params;
 
-	const students = await StudentAcademicYear.find({ academicYear }).populate(
-		'student'
-	);
+	// const students = await StudentAcademicYear.find({
+	// 	academicYear: academicYearID,
+	// })
+	// 	.sort({ level: 1 })
+	// 	.populate('student');
+
+	const students = await StudentAcademicYear.aggregate([
+		{ $match: { academicYear: new mongoose.Types.ObjectId(academicYearID) } },
+		{
+			$lookup: {
+				from: 'students',
+				localField: 'student',
+				foreignField: '_id',
+				as: 'student',
+			},
+		},
+		{ $unwind: '$student' },
+		{
+			$lookup: {
+				from: 'specialties',
+				localField: 'student.specialty',
+				foreignField: '_id',
+				as: 'student.specialty',
+			},
+		},
+		{ $unwind: '$student.specialty' },
+		{
+			$lookup: {
+				from: 'departments',
+				localField: 'student.specialty.department',
+				foreignField: '_id',
+				as: 'student.specialty.department',
+			},
+		},
+		{ $unwind: '$student.specialty.department' },
+		{
+			$lookup: {
+				from: 'programs',
+				localField: 'student.specialty.department.program',
+				foreignField: '_id',
+				as: 'student.specialty.department.program',
+			},
+		},
+		{ $unwind: '$student.specialty.department.program' },
+		{
+			$lookup: {
+				from: 'staffs',
+				localField: 'student.specialty.department.program.director',
+				foreignField: '_id',
+				as: 'student.specialty.department.program.director',
+			},
+		},
+		{ $unwind: '$student.specialty.department.program.director' },
+		{
+			$lookup: {
+				from: 'staffs',
+				localField: 'student.specialty.department.program.deputyDirector',
+				foreignField: '_id',
+				as: 'student.specialty.department.program.deputyDirector',
+			},
+		},
+		{
+			$unwind: '$student.specialty.department.program.deputyDirector',
+		},
+		{
+			$project: {
+				level: 1,
+				'student._id': 1,
+				'student.name': 1,
+				'student.matricule': 1,
+				'student.address': 1,
+				'student.gender': 1,
+				'student.dob': 1,
+				'student.pob': 1,
+				'student.email': 1,
+				'student.tel': 1,
+				'student.parent_name': 1,
+				'student.parent_email': 1,
+				'student.parent_tel': 1,
+				'student.level': 1,
+				'student.entry_certificate': 1,
+				// 'student.specialty': 1,
+				'student.specialty.name': 1,
+				'student.specialty._id': 1,
+				// 'student.specialty.department': 1,
+				'student.specialty.department.name': 1,
+				'student.specialty.department._id': 1,
+				// 'student.specialty.department.hod': 1,
+				// 'student.specialty.department.hod.name': 1,
+				// 'student.specialty.department.hod._id': 1,
+				// 'student.specialty.department.program': 1,
+				'student.specialty.department.program.name': 1,
+				'student.specialty.department.program._id': 1,
+				'student.specialty.department.program.director.name': 1,
+				'student.specialty.department.program.director._id': 1,
+				'student.specialty.department.program.deputyDirector.name': 1,
+				'student.specialty.department.program.deputyDirector._id': 1,
+			},
+		},
+		{ $sort: { level: 1, 'student.name': 1 } },
+	]);
 
 	let allStudents = students.map((stud) => {
 		const student = stud.student;
@@ -111,3 +212,45 @@ exports.getStudentPerAcademicYear = catchAsync(async (req, res, next) => {
 
 	sendResponse(res, 'success', 200, allStudents);
 });
+
+// {
+// 	$project: {
+// 			level: 1,
+// 			student: {
+// 					_id: 1,
+// 					name: 1,
+// 					matricule: 1,
+// 					address: 1,
+// 					gender: 1,
+// 					dob: 1,
+// 					pob: 1,
+// 					email: 1,
+// 					tel: 1,
+// 					parent_name: 1,
+// 					parent_email: 1,
+// 					parent_tel: 1,
+// 					level: 1,
+// 					entry_certificate: 1,
+// 					specialty: {
+// 							name: 1,
+// 							_id: 1,
+// 							department: {
+// 									name: 1,
+// 									_id: 1,
+// 									program: {
+// 											name: 1,
+// 											_id: 1,
+// 											director: {
+// 													name: 1,
+// 													_id: 1,
+// 											},
+// 											deputyDirector: {
+// 													name: 1,
+// 													_id: 1,
+// 											},
+// 									},
+// 							},
+// 					},
+// 			},
+// 	},
+// },
