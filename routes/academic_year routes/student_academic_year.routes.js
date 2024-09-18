a1db93e94bb97188cc76559eb6aff49f2b2e7550
clintjeff2 <clintjeff2@gmail.com>
2024-09-19 00:43:55 +0100

    Edited most routes, to take in academic year id as parameter

--- routes/academic_year routes/student_academic_year.routes.js
+++ routes/academic_year routes/student_academic_year.routes.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const authController = require('../../controllers/authentication/auth.controller');
-const StudentAcademicYear = require('./../../controllers/academic_year controller/student_academic_year.controller');
+const studentAuthController = require('./../../controllers/authentication/auth.student.controller');
+const studentAcademicYear = require('./../../controllers/academic_year controller/student_academic_year.controller');
 
 const router = express.Router();
 
 router
 	.route('/:nextAcademicYearID/:tokenID')
-	.delete(authController.protect, StudentAcademicYear.deletePromotedStudent);
+	.delete(authController.protect, studentAcademicYear.deletePromotedStudent);
 router
 	.route('/bulk-insert/:tokenID')
 	.post(
 		authController.protect,
-		StudentAcademicYear.createStudentAcademicYearBulk
+		studentAcademicYear.createStudentAcademicYearBulk
 	);
 
 router
 	.route('/:academicYearID/:tokenID')
-	.get(authController.protect, StudentAcademicYear.getStudentPerAcademicYear);
+	.get(authController.protect, studentAcademicYear.getStudentPerAcademicYear);
 
 router
 	.route('/:academicYearID/promote-student/:tokenID')
-	.post(authController.protect, StudentAcademicYear.promoteStudent);
+	.post(authController.protect, studentAcademicYear.promoteStudent);
 
 router
 	.route('/:academicYearID/bulk-promote/:tokenID')
-	.post(authController.protect, StudentAcademicYear.promoteStudentsBulk);
+	.post(authController.protect, studentAcademicYear.promoteStudentsBulk);
+
+router
+	.route('/:studentId/all-years/:tokenID')
+	.get(
+		studentAuthController.protect,
+		studentAcademicYear.getStudentAccrossYears
+	);
 module.exports = router;
