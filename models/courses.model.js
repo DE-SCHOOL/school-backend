720befe6735836ee0f5d57d3a1f61654a20f3ee7
clintjeff2 <clintjeff2@gmail.com>
2024-11-15 16:11:35 +0100

    Added course creation to include all the new levels in error message

--- models/courses.model.js
+++ models/courses.model.js
@@ -29,8 +29,8 @@ const courseSchema = new mongoose.Schema({
 		{
 			type: Number,
 			enum: {
-				values: [200, 300, 400, 601, 602],
-				message: 'A level must either be 200, 300, 400, 601, or 602',
+				values: [100, 200, 300, 400, 500, 601, 602],
+				message: 'A level must either be 100, 200, 300, 400, 500, 601, or 602',
 			},
 			required: [true, 'A course is tought in a particular class level'],
 		},
