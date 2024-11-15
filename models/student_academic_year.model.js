7ae4d194ee38e4ef5707a67b9de49fd7495e9aae
clintjeff2 <clintjeff2@gmail.com>
2024-11-15 16:17:18 +0100

    Modified the values that levels can take

--- models/student_academic_year.model.js
+++ models/student_academic_year.model.js
@@ -14,10 +14,10 @@ const studentAcademicYearSchema = new mongoose.Schema({
 	level: {
 		type: Number,
 		enum: {
-			values: [200, 300, 400, 500, 601, 602, 603],
-			message: 'A level must either be 200, 300, 400, 601, or 602',
+			values: [100, 200, 300, 400, 500, 601, 602, 603],
+			message: 'A class must either be 100, 200, 300, 400, 500, 601, or 602',
 		},
-		required: [true, 'A course is tought in a particular class level'],
+		required: [true, 'A student must belong to a class'],
 	},
 	createdAt: {
 		type: Date,
