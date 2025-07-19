2e751abf7fe25e62aa05dd13ca33eaa738736d8a
clintjeff2 <clintjeff2@gmail.com>
2025-07-19 14:20:03 +0100

    Enhance students.model: update schema fields and embed reference integrity

--- models/students.model.js
+++ models/students.model.js
@@ -85,8 +85,8 @@ const studentSchema = new mongoose.Schema({
 	level: {
 		type: Number,
 		enum: {
-			values: [100, 200, 300, 400, 500, 601, 602, 603],
-			message: 'A class must either be 100, 200, 300, 400, 500, 601, or 602',
+			values: [100, 200, 300, 400, 500, 601, 602, 603, 604],
+			message: 'A class must either be 100, 200, 300, 400, 500, 601, 602 or 603',
 		},
 		required: [true, 'A student belongs to a particular class'],
 	},
